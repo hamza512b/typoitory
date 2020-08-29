cafb3cc93e694ccad172c82a8b1d3af60419b777
Hamza Khuswan <hamza@golinears.com>
2020-08-29 17:10:13 +0200

    int valitions + colored text

--- typoitory.c
+++ typoitory.c
@@ -2,18 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-// #include <sys/time.h>
+#include <inttypes.h>
+#include <errno.h>
 
-char *words[9894];
+#define RED  "\x1B[31m"
+#define GRN  "\x1B[32m"
+#define NRM  "\x1B[0m"
+
+#define MAX_INPUT 50 // TODO egemntation fault if is bigger
+#define N 9894
+
+char *words[N];
+
+// Prototypes
+int getInt(char *txt);
 
 int main()
 {
-    int num;
-    printf("How many words? ");
-    scanf("%i", &num);
+    int num = 0;
 
-    // struct timeval stop, start;
-    // gettimeofday(&start, NULL);
+    while (num == 0)
+    {
+        num = getInt("How many words? ");
+    }
+
     time_t start;
     time(&start);
 
@@ -39,33 +51,49 @@ int main()
     for (int i = 0; i < num; i++)
     {
         srand(time(NULL) + i);
-        int ran = rand() % 9893;
+        int ran = rand() % (N - 1);
         printf("%s\n", words[ran]);
 
-        char name[50]; // Segemntation fault if is bigger then 50
+        char name[MAX_INPUT];
         scanf("%s", name);
 
         if (strcmp(name, words[ran]) == 0)
         {
-            printf("Correct\n\n");
+            printf("%sCorrect\n\n%s", GRN, NRM);
             ++correct_words;
         }
         else {
-            printf("False\n\n");
+            printf("%sFalse\n\n%s", RED, NRM);
         }
     }
-
-    // gettimeofday(&start, NULL);
-    // double secs = (double)(stop.tv_usec - start.tv_usec) / 1000 + (double)(stop.tv_sec - start.tv_sec);
-    // printf("%f\n", secs);
 
     time_t end;
     time(&end);
     int secs = (int)(end - start);
     printf("Correct:  %iw\nTime:     %is\n%.0fw/s\n", correct_words, secs, (float)correct_words / (float)(secs / 60.0));
 
-    for (int j = 0; j < 9894; j++)
+    for (int j = 0; j < 10000; j++)
     {
         free(words[j]);
     }
+}
+
+
+int getInt(char *txt)
+{
+    printf("%s", txt);
+
+    char str[MAX_INPUT];
+    scanf("%s", str);
+    char *str_r;
+
+    if (str == NULL)
+        return 0;
+
+    int num = (int) strtol(str, &str_r, 10);
+
+    if (*str_r != '\0')
+        return 0;
+
+    return num;
 }
